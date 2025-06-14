596db188f029ccdf498195d6cb11ee43a3861cb1
Joia <gustavo.sc.joia@gmail.com>
2025-06-14 00:17:18 -0300

    teste para render

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="{{asset('setup/vue.global.js')}}"></script>
-    <script src="{{asset('setup/vue-router.global.js')}}"></script>
-    <script src="{{asset('setup/bootstrap.bundle.min.js')}}" defer></script>
-    <link rel="stylesheet" href="{{asset('style/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('style/nouislider.min.css')}}">
-    <link rel="stylesheet/less" type="text/css" href="{{asset('style/style.less')}}" />
-    <script src="{{asset('setup/less.js')}}" type="text/javascript"></script>
-    <script src="{{asset('setup/sweetalert.js')}}"></script>
-    <script src="{{asset('setup/chart.js')}}"></script>
-    <script src="{{asset('setup/xlsx.full.min.js')}}"></script>
-    <script src="{{asset('setup/nouislider.min.js')}}"></script>
+    <script src="/setup/vue.global.js"></script>
+    <script src="/setup/vue-router.global.js"></script>
+    <script src="/setup/bootstrap.bundle.min.js" defer></script>
+    <link rel="stylesheet" href="/style/bootstrap.min.css">
+    <link rel="stylesheet" href="/style/nouislider.min.css">
+    <link rel="stylesheet/less" type="text/css" href="/style/style.less" />
+    <script src="/setup/less.js" type="text/javascript"></script>
+    <script src="/setup/sweetalert.js"></script>
+    <script src="/setup/chart.js"></script>
+    <script src="/setup/xlsx.full.min.js"></script>
+    <script src="/setup/nouislider.min.js"></script>
     <title>Leituras do Radiômetro</title>
 </head>
 <body>
@@ -21,6 +21,6 @@
     <div id="app"></div>
 
     <!--Importando o script que contém a aplicação do VueJs em prioritário-->
-    <script type="module" src="{{asset('js/app.js')}}" defer></script>
+    <script type="module" src="/js/app.js" defer></script>
 </body>
 </html>
