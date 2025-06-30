c5f25eaf424abf03b01a523cf7a728596d16bcba
Joia <gustavo.sc.joia@gmail.com>
2025-06-30 01:14:00 -0300

    FEAT: Preparação para a página de exibição para os dados coletados manualmente

--- public/js/components/sidebar.js
+++ public/js/components/sidebar.js
@@ -8,7 +8,7 @@ export const Sidebar = {
                     <li><router-link to="/temps">Leitura de Temperatura</router-link></li>
                     <li><router-link to="/umi">Leitura de Umidade</router-link></li>
                     <li><router-link to="/press">Leitura de Pressão Barométrica</router-link></li>
-                    <li><router-link to="/plan">Leitura de Planilha</router-link></li>
+                    <li><router-link to="/plan">Leitura da Planilha</router-link></li>
                 </ul>
             </nav>
         </aside>
