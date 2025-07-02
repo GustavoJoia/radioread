48dce738409d4de5b8670a61aff45cc4ff54a1c1
Joia <gustavo.sc.joia@gmail.com>
2025-07-02 13:24:44 -0300

    FEAT: Tabela de amostra dos dados

--- public/js/pages/upload.js
+++ public/js/pages/upload.js
@@ -1,17 +1,48 @@
 export const Upload = {
     template:`
         <h1>Envio de dados</h1><h5>via arquivo .txt</h5>
-        <input v-model="arquivo" @change="lerArquivo" type="file" name="txtdados" id="txtdados" accept=".txt,.csv">
-        <button name="submit" id="submit" type="submit" @click="gravar">Gravar</button>
+        <div class="input-group mb-3">
+            <input v-model="arquivo" @change="lerArquivo" name="txtdados" id="txtdados" accept=".txt" type="file" class="form-control" placeholder="Seu arquivo .txt" aria-label="Seu arquivo .txt" aria-describedby="submit">
+            <div class="input-group-append">
+                <button class="btn btn-secondary" type="button" id="submit" @click="gravar">Gravar</button>
+            </div>
+        </div>
+        
 
-        <h3>Conteúdo do arquivo</h3>
-        <p>{{linhas_dados}}</p>
+        <h4 v-show="preview!=null">Dados a serem gravados:</h4>
+        <table v-show="preview!=null" class="table">
+            <thead class="thead-light">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Temp 1 (C)</th>
+                    <th scope="col">Temp 2 (C)</th>
+                    <th scope="col">Temp 3 (C)</th>
+                    <th scope="col">Temp 4 (C)</th>
+                    <th scope="col">Umid (%)</th>
+                    <th scope="col">P.A. (Pa)</th>
+                    <th scope="col">Altit (m)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(linha, index) in preview" :key="index">
+                    <th scope="row">{{index+1}}</th>
+                    <td>{{linha[0]}}</td>
+                    <td>{{linha[1]}}</td>
+                    <td>{{linha[2]}}</td>
+                    <td>{{linha[3]}}</td>
+                    <td>{{linha[4]}}</td>
+                    <td>{{linha[5]}}</td>
+                    <td>{{linha[6]}}</td>
+                </tr>
+            </tbody>
+        </table>
     `,
 
     data(){
         return{
             arquivo:null,
             linhas_dados: null,
+            preview:null,
             leitor: new FileReader()
         }
     },
@@ -23,16 +54,27 @@ export const Upload = {
                 const conteudo = e.target.result;
                 this.arquivo = conteudo;
                 let linhas = conteudo.split(';');
-                linhas = linhas.slice(0,-1);
+                linhas.pop();
+                this.preview = [];
+                linhas.forEach(linha => {
+                    let split = linha.split(',')
+                    for (let index = 0; index < split.length; index++) {
+                        split[index] = parseFloat(split[index]);
+                    }
+                    this.preview.push(split);
+                });
+                console.log(this.preview)
                 this.linhas_dados = JSON.stringify(linhas);
             }
             this.leitor.readAsText(arquivo);
         },
         quebrarLinhas(){
             let linhas = this.arquivo.split(';');
+            console.log(linhas);
             this.linhas_dados = JSON.stringify(linhas);
         },
         gravar(){
+            document.querySelector('#submit').disabled = true;
             let options = {
                 method: 'POST',
                 headers:{
@@ -44,7 +86,12 @@ export const Upload = {
             fetch(`${server}/api/sensor/gravar/arquivo`,options)
             .then(response=>response.json())
             .then(response=>{
-                console.log(response)
+                Swal.fire({
+                    text:response.message,
+                    allowOutsideClick: false,
+                    allowEscapeKey: false
+                });
+                document.querySelector('#submit').disabled = false;
             })
         }
     },
