c267e0d44983b94ec606b0ae77ac80fd078ceeb1
Joia <gustavo.sc.joia@gmail.com>
2025-08-23 12:56:15 -0300

    (incompleto) Filtro

--- public/js/pages/leds.js
+++ public/js/pages/leds.js
@@ -9,6 +9,42 @@ export const Leds = {
                 <option value="c">Potência total (uW)</option>
             </select>
         </div>
+        <div class="row mb-3">
+            <div class="col">
+                <label>Ano:</label>
+                <select v-model="selectedAno" @change="atualizarMesesDias; rendererFiltrado()" class="form-select">
+                    <option v-for="ano in anosDisponiveis" :key="ano">{{ano}}</option>
+                </select>
+            </div>
+            <div class="col">
+                <label>Mês:</label>
+                <select v-model="selectedMes" @change="atualizarMesesDias; rendererFiltrado()" class="form-select">
+                    <option v-for="mes in mesesDisponiveis" :key="mes">{{mes}}</option>
+                </select>
+            </div>
+            <div class="col">
+                <label>Dia:</label>
+                <select v-model="selectedDia" @change="rendererFiltrado()" class="form-select">
+                    <option v-for="dia in diasDisponiveis" :key="dia">{{dia}}</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <div class="col">
+                <label>Hora Início:</label>
+                <select v-model="selectedHoraInicio" @change="rendererFiltrado()" class="form-select">
+                    <option v-for="h in horasDisponiveis" :key="h">{{h}}</option>
+                </select>
+            </div>
+            <div class="col">
+                <label>Hora Fim:</label>
+                <select v-model="selectedHoraFim" @change="rendererFiltrado()" class="form-select">
+                    <option v-for="h in horasDisponiveis" :key="h">{{h}}</option>
+                </select>
+            </div>
+        </div>
+
         <canvas v-show="chart=='a'" id="tensao"></canvas>
         <canvas v-show="chart=='b'" id="corrente"></canvas>
         <canvas v-show="chart=='c'" id="totais"></canvas>
@@ -29,6 +65,17 @@ export const Leds = {
                 am:80000
             },
 
+            selectedAno: null,
+            selectedMes: null,
+            selectedDia: null,
+            selectedHoraInicio: null,
+            selectedHoraFim: null,
+
+            anosDisponiveis: [],
+            mesesDisponiveis: [],
+            diasDisponiveis: [],
+            horasDisponiveis: [],
+
             documents:[],
             datas:[],
             tensao_vm:[],
@@ -47,6 +94,108 @@ export const Leds = {
     },
 
     methods:{
+
+        gerarHoras() {
+            const horas = [];
+            for (let h = 0; h < 24; h++) {
+                for (let m = 0; m < 60; m += 15) {
+                    const hh = h.toString().padStart(2, '0');
+                    const mm = m.toString().padStart(2, '0');
+                    horas.push(`${hh}:${mm}`);
+                }
+            }
+            this.horasDisponiveis = horas;
+            this.selectedHoraInicio = horas[0];
+            this.selectedHoraFim = horas[horas.length-1];
+        },
+        gerarDatasDisponiveis() {
+            const anos = new Set();
+            const meses = new Set();
+            const dias = new Set();
+
+            this.documents.forEach(d => {
+                const date = new Date(d.data_hora);
+                anos.add(date.getFullYear());
+            });
+
+            this.anosDisponiveis = Array.from(anos).sort((a,b)=>a-b);
+            this.selectedAno = this.anosDisponiveis[0];
+
+            this.atualizarMesesDias();
+        },
+        atualizarMesesDias() {
+            console.log(this.anosDisponiveis)
+            const meses = new Set();
+            const dias = new Set();
+
+            this.documents.forEach(d => {
+                const date = new Date(d.data_hora);
+                if (date.getFullYear() === this.selectedAno) {
+                    meses.add(date.getMonth() + 1); // mês de 1 a 12
+                }
+            });
+
+            this.mesesDisponiveis = Array.from(meses).sort((a,b)=>a-b);
+            this.selectedMes = this.mesesDisponiveis[0];
+
+            this.documents.forEach(d => {
+                const date = new Date(d.data_hora);
+                if (date.getFullYear() === this.selectedAno && (date.getMonth()+1) === this.selectedMes) {
+                    dias.add(date.getDate());
+                }
+            });
+
+            this.diasDisponiveis = Array.from(dias).sort((a,b)=>a-b);
+            this.selectedDia = this.diasDisponiveis[0];
+        },
+        filtrarPorDataEHora() {
+            const dataInicio = new Date(`${this.selectedAno}-${this.selectedMes.toString().padStart(2,'0')}-${this.selectedDia.toString().padStart(2,'0')}T${this.selectedHoraInicio}:00`);
+            const dataFim = new Date(`${this.selectedAno}-${this.selectedMes.toString().padStart(2,'0')}-${this.selectedDia.toString().padStart(2,'0')}T${this.selectedHoraFim}:59`);
+
+            const filtrados = this.documents.filter(d => {
+                const dt = new Date(d.data_hora);
+                return dt >= dataInicio && dt <= dataFim;
+            });
+
+            return filtrados;
+        },
+        rendererFiltrado() {
+            const filtrados = this.filtrarPorDataEHora();
+
+            // Limpa arrays
+            this.datas = [];
+            this.tensao_am = [];
+            this.tensao_az = [];
+            this.tensao_vd = [];
+            this.tensao_vm = [];
+            this.total_tensao = [];
+            this.corrente_am = [];
+            this.corrente_az = [];
+            this.corrente_vd = [];
+            this.corrente_vm = [];
+            this.total_corrente = [];
+            this.total_potencia = [];
+
+            filtrados.forEach(leitura => {
+                this.datas.push(leitura['data_hora']);
+                this.tensao_am.push(leitura['tensaoAM']);
+                this.tensao_az.push(leitura['tensaoAZ']);
+                this.tensao_vd.push(leitura['tensaoVD']);
+                this.tensao_vm.push(leitura['tensaoVM']);
+                this.total_tensao.push((this.tensao_am.at(-1))+(this.tensao_az.at(-1))+(this.tensao_vd.at(-1))+(this.tensao_vm.at(-1)));
+                
+                this.corrente_am.push((leitura['tensaoAM']*this.resistores.am)/1000);
+                this.corrente_az.push((leitura['tensaoAZ']*this.resistores.az)/1000);
+                this.corrente_vd.push((leitura['tensaoVD']*this.resistores.vd)/1000);
+                this.corrente_vm.push((leitura['tensaoVM']*this.resistores.vm)/1000);
+                this.total_corrente.push((this.corrente_am.at(-1))+(this.corrente_az.at(-1))+(this.corrente_vd.at(-1))+(this.corrente_vm.at(-1)));
+
+                this.total_potencia.push((this.total_corrente.at(-1))+(this.total_tensao.at(-1)));
+            });
+
+            // Atualiza o gráfico
+            this.renderer();
+        },
         requestTemp(){
             Swal.showLoading();
             let server = window.location.origin;
@@ -54,7 +203,7 @@ export const Leds = {
             .then(response=>response.json())
             .then(response=>{
                 this.documents = response
-                this.renderer()
+                this.rendererFiltrado()
             })
         },
         renderer(){
@@ -152,5 +301,6 @@ export const Leds = {
     },
     mounted(){
         this.requestTemp();
+        this.gerarHoras();
     }
 }
